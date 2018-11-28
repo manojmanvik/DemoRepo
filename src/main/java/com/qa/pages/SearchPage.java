d715ffbbb5939e0938ad8f8c59c4ee3f1b6aa154
manojmanvik <41783476+manojmanvik@users.noreply.github.com>
2018-11-28 20:10:40 +0530

    Update SearchPage.java

--- src/main/java/com/qa/pages/SearchPage.java
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -3,9 +3,9 @@ package com.qa.pages;
 public class SearchPage {
 
 	
-	public void searchpage() {
+	public void search1page() {
 		
-		System.out.println("searchpage");
+		System.out.println("search1page");
 		
 	}
 		
